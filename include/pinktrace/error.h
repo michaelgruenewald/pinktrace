6a710e15cf9af9de3de4ebd0a8deebadd462722d
Ali Polatel <alip@exherbo.org>
2010-04-21 01:10:28 +0300

    Cleaner error handling

---
+++ include/pinktrace/error.h
@@ -0,0 +1,36 @@
+/* vim: set cino= fo=croql sw=8 ts=8 sts=0 noet cin fdm=syntax : */
+
+/*
+ * Copyright (c) 2010 Ali Polatel <alip@exherbo.org>
+ *
+ * This file is part of Pink's Tracing Library. pinktrace is free software; you
+ * can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License version 2.1, as published by the Free Software
+ * Foundation.
+ *
+ * pinktrace is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PINKTRACE_GUARD_ERROR_H
+#define PINKTRACE_GUARD_ERROR_H 1
+
+/**
+ * \file
+ * Pink's error constants
+ **/
+
+enum pink_error {
+	PINK_ERROR_SUCCESS = 0,	/*<< Success */
+	PINK_ERROR_FORK,	/*<< Fork failed */
+	PINK_ERROR_TRACE,	/*<< pink_trace_me() failed */
+	PINK_ERROR_TRACE_SETUP,	/*<< pink_trace_setup() failed */
+};
+
+#endif /* !PINKTRACE_GUARD_ERROR_H */
