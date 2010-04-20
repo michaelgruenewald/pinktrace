5ae86c32ff70c15b33acc2dc1ab95b24d20bcd75
Ali Polatel <alip@exherbo.org>
2010-04-21 02:21:50 +0300

    Initial support for event handling

---
+++ include/pinktrace/event.h
@@ -0,0 +1,54 @@
+/* vim: set cino= fo=croql sw=8 ts=8 sts=0 noet cin fdm=syntax : */
+
+/*
+ * Copyright (c) 2010 Ali Polatel <alip@exherbo.org>
+ *
+ * This file is part of Pink's Tracing Library. pinktrace is free software; you
+ * can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License version 2.1, as published by the Free Software
+ * Foundation.
+ *
+ * pinktrace is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PINKTRACE_GUARD_ERROR_H
+#define PINKTRACE_GUARD_ERROR_H 1
+
+/**
+ * \file
+ * Pink's event handling
+ **/
+
+enum pink_event {
+	PINK_EVENT_STOP,	/*<< Child has been stopped */
+	PINK_EVENT_SYSCALL,	/*<< Child has entered/exited a system call */
+	PINK_EVENT_FORK,	/*<< Child called fork() */
+	PINK_EVENT_VFORK,	/*<< Child called vfork() */
+	PINK_EVENT_CLONE,	/*<< Child called clone() */
+	PINK_EVENT_VFORK_DONE,	/*<< Child is exiting a vfork() call */
+	PINK_EVENT_EXEC,	/*<< Child called execve() */
+	PINK_EVENT_EXIT,	/*<< Child is exiting (ptrace way, stopped before exit) */
+	PINK_EVENT_GENUINE,	/*<< Child has received a genuine signal */
+	PINK_EVENT_EXIT_GENUINE,/*<< Child has *exited* normally */
+	PINK_EVENT_EXIT_SIGNAL, /*<< Child exited with a signal */
+	PINK_EVENT_UNKNOWN,	/*<< Unknown event, shouldn't happen */
+};
+
+/**
+ * Return the last event made by child.
+ *
+ * \param ctx The tracing context
+ * \param status The status argument, received from wait(2) system call.
+ *
+ * \return One of PINK_EVENT_* constants
+ **/
+unsigned int
+pink_event(pink_context_t *ctx, int status);
+#endif /* !PINKTRACE_GUARD_EVENT_H */
---
+++ src/pink-event.c
@@ -0,0 +1,72 @@
+/* vim: set cino= fo=croql sw=8 ts=8 sts=0 noet cin fdm=syntax : */
+
+/*
+ * Copyright (c) 2010 Ali Polatel <alip@exherbo.org>
+ *
+ * This file is part of Pink's Tracing Library. pinktrace is free software; you
+ * can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License version 2.1, as published by the Free Software
+ * Foundation.
+ *
+ * pinktrace is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <assert.h>
+#include <sys/wait.h>
+
+#include <pinktrace/internal.h>
+#include <pinktrace/event.h>
+#include <pinktrace/context.h>
+#include <pinktrace/trace.h>
+
+unsigned int
+pink_event(pink_context_t *ctx, int status)
+{
+	unsigned int event;
+
+	/* TODO: Right now this function assumes PTRACE_O_TRACESYSGOOD is set.
+	 * It might be a good idea to support event decisions when this flag
+	 * isn't set.
+	 */
+	assert(ctx->options & PINK_TRACE_OPTION_SYSGOOD);
+
+	if (WIFSTOPPED(status)) {
+		switch (WSTOPSIG(status)) {
+		case SIGSTOP:
+			return PINK_EVENT_STOP;
+		case SIGTRAP | 0x80:
+			return PINK_EVENT_SYSCALL;
+		default:
+			event = (status >> 16) & 0xffff;
+			switch (event) {
+			case PTRACE_EVENT_FORK:
+				return PINK_EVENT_FORK;
+			case PTRACE_EVENT_VFORK:
+				return PINK_EVENT_VFORK;
+			case PTRACE_EVENT_CLONE:
+				return PINK_EVENT_CLONE;
+			case PTRACE_EVENT_VFORK_DONE:
+				return PINK_EVENT_VFORK_DONE;
+			case PTRACE_EVENT_EXEC:
+				return PINK_EVENT_EXEC;
+			case PTRACE_EVENT_EXIT:
+				return PINK_EVENT_EXIT;
+			default:
+				return PINK_EVENT_GENUINE;
+			}
+		}
+	}
+	else if (WIFEXITED(status))
+		return PINK_EVENT_EXIT_GENUINE;
+	else if (WIFSIGNALED(status))
+		return PINK_EVENT_EXIT_SIGNAL;
+
+	return PINK_EVENT_UNKNOWN;
+}
