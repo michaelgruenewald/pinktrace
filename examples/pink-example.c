b02dcee8a2159f016bf378db22e339d75a9bfac7
Ali Polatel <alip@exherbo.org>
2010-04-23 21:46:10 +0300

    Remove the now obsolete description

--- examples/pink-example.c
+++ examples/pink-example.c
@@ -18,11 +18,6 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/**
- * \file
- * Example describing the low level API
- **/
-
 #include <errno.h>
 #include <stdbool.h>
 #include <sys/types.h>
