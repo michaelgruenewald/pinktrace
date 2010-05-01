ca8fef1371c35e60931a9192b0c4a3147003a245
Ali Polatel <alip@exherbo.org>
2010-05-01 22:56:33 +0300

    python: More preparation

---
+++ python/pinktrace.pyx
@@ -0,0 +1,5 @@
+# vim: set sw=4 ts=4 sts=4 et tw=80 :
+# Copyright 2010 Ali Polatel <alip@exherbo.org>
+# Distributed under the terms of the GNU Lesser General Public License v2
+
+# Process this file with Cython.
