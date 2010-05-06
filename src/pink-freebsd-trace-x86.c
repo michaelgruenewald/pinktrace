ca3554bbb5042914f0fb6beb8bb866e8ac6c54e7
Ali Polatel <alip@exherbo.org>
2010-05-06 14:40:50 +0300

    freebsd: Add more utility functions

--- src/pink-freebsd-trace-x86.c
+++ src/pink-freebsd-trace-x86.c
@@ -18,7 +18,9 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <assert.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <sys/types.h>
 
 #include <pinktrace/internal.h>
@@ -50,6 +52,74 @@ pink_util_set_syscall(pid_t pid, long scno)
 
 	if (0 > ptrace(PT_SETREGS, pid, (caddr_t)&r, 0))
 		return false;
+
+	return true;
+}
+
+bool
+pink_util_get_return(pid_t pid, long *res)
+{
+	struct reg r;
+
+	if (0 > ptrace(PT_GETREGS, pid, (caddr_t)&r, 0))
+		return false;
+
+	if (res)
+		*res = r.r_eax;
+
+	return true;
+}
+
+bool
+pink_util_set_return(pid_t pid, long ret)
+{
+	struct reg r;
+
+	if (0 > ptrace(PT_GETREGS, pid, (caddr_t)&r, 0))
+		return false;
+
+	r.r_eax = ret;
+
+	if (0 > ptrace(PT_SETREGS, pid, (caddr_t)&r, 0))
+		return false;
+
+	return true;
+}
+
+bool
+pink_util_get_arg(pid_t pid, pink_unused pink_bitness_t bitness, unsigned ind, long *res)
+{
+	struct reg r;
+
+	assert(ind < PINK_MAX_INDEX);
+
+	if (0 > ptrace(PT_GETREGS, pid, (caddr_t)&r, 0))
+		return false;
+
+	if (res) {
+		switch (ind) {
+		case 0:
+			*res = r.r_ebx;
+			break;
+		case 1:
+			*res = r.r_ecx;
+			break;
+		case 2:
+			*res = r.r_edx;
+			break;
+		case 3:
+			*res = r.r_esi;
+			break;
+		case 4:
+			*res = r.r_edi;
+			break;
+		case 5:
+			*res = r.r_ebp;
+			break;
+		default:
+			abort();
+		}
+	}
 
 	return true;
 }
