michaelgruenewald/pinktrace

commit 5ff446dcdacf92bcc6c71cfc1fb431a741452d0a

freebsd: add the ability to trace 32bit binaries on amd64

src/pink-freebsd-trace-i386.c
@@ -36,7 +36,7 @@ pink_bitness_get(pink_unused pid_t pid)
 }
 
 bool
-pink_util_get_syscall(pid_t pid, long *res)
+pink_util_get_syscall(pid_t pid, pink_unused pink_bitness_t bitness, long *res)
 {
 	unsigned parm_offset;
 	struct reg r;

examples/c/pink-simple-strace-freebsd.c
@@ -16,6 +16,14 @@
 
 #define MAX_STRING_LEN 128
 
+struct child {
+	pid_t pid;
+	pink_bitness_t bitness;
+	bool insyscall;
+	bool inexecve;
+	bool printret;
+};
+
 /* Utility functions */
 static void
 print_ret(long ret)
@@ -60,21 +68,6 @@ print_open_flags(long flags)
 		printf("%#x", (unsigned)flags);
 }
 
-/* A generic decoder for system calls. */
-static void
-decode_simple(pink_bitness_t bitness, long scno)
-{
-	const char *scname;
-
-	/* Figure out the name of the system call. */
-	scname = pink_name_syscall(scno, bitness);
-	if (scname == NULL)
-		printf("%ld", scno);
-	else
-		printf("%s", scname);
-	printf("()");
-}
-
 /* A very basic decoder for open(2) system call. */
 static void
 decode_open(pid_t pid, pink_bitness_t bitness)
@@ -96,14 +89,66 @@ decode_open(pid_t pid, pink_bitness_t bitness)
 	fputc(')', stdout);
 }
 
+static void
+handle_sigtrap(struct child *son)
+{
+	long scno, ret;
+	const char *scname;
+
+	/* We get this event twice, one at entering a
+	 * system call and one at exiting a system
+	 * call. */
+	if (son->insyscall) {
+		if (!pink_util_get_return(son->pid, &ret))
+			err(EXIT_FAILURE, "pink_util_get_return");
+		if (son->inexecve) {
+			son->inexecve = false;
+			if (ret == 0) { /* execve was successful */
+				/* Update bitness */
+				son->bitness = pink_bitness_get(son->pid);
+				if (son->bitness == PINK_BITNESS_UNKNOWN)
+					err(EXIT_FAILURE, "pink_bitness_get");
+				printf(" = 0 (Updating the bitness of child %i to %s mode)\n",
+					son->pid, pink_bitness_tostring(son->bitness));
+				son->printret = false;
+				return;
+			}
+		}
+		son->insyscall = false;
+		if (!son->printret) {
+			son->printret = true;
+			return;
+		}
+		/* Exiting the system call, print the
+		 * return value. */
+		fputc(' ', stdout);
+		print_ret(ret);
+		fputc('\n', stdout);
+	}
+	else {
+		son->insyscall = true;
+		/* Get the system call number and call
+		 * the appropriate decoder. */
+		if (!pink_util_get_syscall(son->pid, son->bitness, &scno))
+			err(EXIT_FAILURE, "pink_util_get_syscall");
+		scname = pink_name_syscall(scno, son->bitness);
+		assert(scname != NULL);
+
+		if (!strcmp(scname, "execve"))
+			son->inexecve = true;
+
+		if (!strcmp(scname, "open"))
+			decode_open(son->pid, son->bitness);
+		else
+			printf("%s()", scname);
+	}
+}
+
 int
 main(int argc, char **argv)
 {
-	bool inexecve, insyscall;
 	int sig, status, exit_code;
-	long scno, ret;
-	pid_t pid;
-	pink_bitness_t bitness;
+	struct child son;
 
 	/* Parse arguments */
 	if (argc < 2) {
@@ -112,11 +157,11 @@ main(int argc, char **argv)
 	}
 
 	/* Fork */
-	if ((pid = fork()) < 0) {
+	if ((son.pid = fork()) < 0) {
 		perror("fork");
 		return EXIT_FAILURE;
 	}
-	else if (!pid) { /* child */
+	else if (!son.pid) { /* child */
 		/* Set up for tracing */
 		if (!pink_trace_me()) {
 			perror("pink_trace_me");
@@ -131,71 +176,39 @@ main(int argc, char **argv)
 		_exit(-1);
 	}
 	else {
-		waitpid(pid, &status, 0);
+		waitpid(son.pid, &status, 0);
 		assert(WIFSTOPPED(status));
 		assert(WSTOPSIG(status) == SIGSTOP);
 
 		/* Figure out the bitness of the traced child. */
-		bitness = pink_bitness_get(pid);
-		printf("Child %i runs in %s mode\n", pid, pink_bitness_tostring(bitness));
+		son.bitness = pink_bitness_get(son.pid);
+		if (son.bitness == PINK_BITNESS_UNKNOWN)
+			err(EXIT_FAILURE, "pink_bitness_get");
+		printf("Child %i runs in %s mode\n", son.pid, pink_bitness_tostring(son.bitness));
 
-		inexecve = insyscall = false;
+		son.inexecve = son.insyscall = false;
+		son.printret = true;
 		sig = exit_code = 0;
 		for (;;) {
 			/* At this point the traced child is stopped and needs
 			 * to be resumed.
 			 */
-			if (!pink_trace_syscall(pid, sig)) {
+			if (!pink_trace_syscall(son.pid, sig)) {
 				perror("pink_trace_syscall");
 				return (errno == ESRCH) ? 0 : 1;
 			}
 			sig = 0;
 
 			/* Wait for the child */
-			if ((pid = waitpid(pid, &status, 0)) < 0) {
+			if ((son.pid = waitpid(son.pid, &status, 0)) < 0) {
 				perror("waitpid");
 				return (errno == ECHILD) ? 0 : 1;
 			}
 
 			/* Check the event */
 			if (WIFSTOPPED(status)) {
-				if (WSTOPSIG(status) == SIGTRAP) {
-					/* We get this event twice, one at entering a
-					 * system call and one at exiting a system
-					 * call. */
-					if (insyscall) {
-						if (!pink_util_get_return(pid, &ret))
-							err(EXIT_FAILURE, "pink_util_get_return");
-						if (inexecve) {
-							inexecve = false;
-							if (!ret) {
-								/* Update bitness */
-								bitness = pink_bitness_get(pid);
-								continue;
-							}
-						}
-						/* Exiting the system call, print the
-						 * return value. */
-						fputc(' ', stdout);
-						print_ret(ret);
-						fputc('\n', stdout);
-						insyscall = false;
-					}
-					else {
-						/* Get the system call number and call
-						 * the appropriate decoder. */
-						if (!pink_util_get_syscall(pid, &scno))
-							err(EXIT_FAILURE, "pink_util_get_syscall");
-						if (scno == SYS_execve)
-							inexecve = true;
-
-						if (scno == SYS_open)
-							decode_open(pid, bitness);
-						else
-							decode_simple(bitness, scno);
-						insyscall = true;
-					}
-				}
+				if (WSTOPSIG(status) == SIGTRAP)
+					handle_sigtrap(&son);
 				else {
 					/* Child received a genuine signal.
 					 * Send it to the child. */
@@ -205,13 +218,13 @@ main(int argc, char **argv)
 			else if (WIFEXITED(status)) {
 				exit_code = WEXITSTATUS(status);
 				printf("Child %i exited normally with return code %d\n",
-						pid, exit_code);
+						son.pid, exit_code);
 				break;
 			}
 			else if (WIFSIGNALED(status)) {
 				exit_code = 128 + WTERMSIG(status);
 				printf("Child %i exited with signal %d\n",
-						pid, WTERMSIG(status));
+						son.pid, WTERMSIG(status));
 				break;
 			}
 		}

examples/c/pink-simple-strace-linux.c
@@ -1,6 +1,7 @@
 /* vim: set cino= fo=croql sw=8 ts=8 sts=0 noet cin fdm=syntax : */
 
 #include <assert.h>
+#include <err.h>
 #include <errno.h>
 #include <sys/syscall.h>
 #include <sys/wait.h>
@@ -14,6 +15,13 @@
 #include <pinktrace/pink.h>
 
 #define MAX_STRING_LEN 128
+
+struct child {
+	pid_t pid;
+	pink_bitness_t bitness;
+	bool insyscall;
+	bool dead;
+};
 
 /* Utility functions */
 static void
@@ -67,21 +75,6 @@ print_open_flags(long flags)
 		printf("%#x", (unsigned)flags);
 }
 
-/* A generic decoder for system calls. */
-static void
-decode_simple(pink_bitness_t bitness, long scno)
-{
-	const char *scname;
-
-	/* Figure out the name of the system call. */
-	scname = pink_name_syscall(scno, bitness);
-	if (scname == NULL)
-		printf("%ld", scno);
-	else
-		printf("%s", scname);
-	printf("()");
-}
-
 /* A very basic decoder for open(2) system call. */
 static void
 decode_open(pid_t pid, pink_bitness_t bitness)
@@ -105,15 +98,47 @@ decode_open(pid_t pid, pink_bitness_t bitness)
 	fputc(')', stdout);
 }
 
+static void
+handle_syscall(struct child *son)
+{
+	long scno;
+	const char *scname;
+
+	/* We get this event twice, one at entering a
+	 * system call and one at exiting a system
+	 * call. */
+	if (son->insyscall) {
+		/* Exiting the system call, print the
+		 * return value. */
+		son->insyscall = false;
+		fputc(' ', stdout);
+		print_ret(son->pid);
+		fputc('\n', stdout);
+	}
+	else {
+		/* Get the system call number and call
+		 * the appropriate decoder. */
+		son->insyscall = true;
+		if (!pink_util_get_syscall(son->pid, son->bitness, &scno)) {
+			perror("pink_util_get_syscall");
+			return;
+		}
+		scname = pink_name_syscall(scno, son->bitness);
+		if (!scname)
+			printf("%ld()", scno);
+		else if (!strcmp(scname, "open"))
+			decode_open(son->pid, son->bitness);
+		else
+			printf("%s()", scname);
+	}
+}
+
 int
 main(int argc, char **argv)
 {
-	bool dead, insyscall;
 	int sig, status, exit_code;
-	long scno;
-	pid_t pid;
-	pink_bitness_t bitness;
 	pink_event_t event;
+	struct child son;
 
 	/* Parse arguments */
 	if (argc < 2) {
@@ -122,11 +147,11 @@ main(int argc, char **argv)
 	}
 
 	/* Fork */
-	if ((pid = fork()) < 0) {
+	if ((son.pid = fork()) < 0) {
 		perror("fork");
 		return EXIT_FAILURE;
 	}
-	else if (!pid) { /* child */
+	else if (!son.pid) { /* child */
 		/* Set up for tracing */
 		if (!pink_trace_me()) {
 			perror("pink_trace_me");
@@ -141,35 +166,37 @@ main(int argc, char **argv)
 		_exit(-1);
 	}
 	else {
-		waitpid(pid, &status, 0);
+		waitpid(son.pid, &status, 0);
 		event = pink_event_decide(status);
 		assert(event == PINK_EVENT_STOP);
 
 		/* Set up the tracing options. */
-		if (!pink_trace_setup(pid, PINK_TRACE_OPTION_SYSGOOD | PINK_TRACE_OPTION_EXEC)) {
+		if (!pink_trace_setup(son.pid, PINK_TRACE_OPTION_SYSGOOD | PINK_TRACE_OPTION_EXEC)) {
 			perror("pink_trace_setup");
-			pink_trace_kill(pid);
+			pink_trace_kill(son.pid);
 			return EXIT_FAILURE;
 		}
 
 		/* Figure out the bitness of the traced child. */
-		bitness = pink_bitness_get(pid);
-		printf("Child %i runs in %s mode\n", pid, pink_bitness_tostring(bitness));
+		son.bitness = pink_bitness_get(son.pid);
+		if (son.bitness == PINK_BITNESS_UNKNOWN)
+			err(EXIT_FAILURE, "pink_bitness_get");
+		printf("Child %i runs in %s mode\n", son.pid, pink_bitness_tostring(son.bitness));
 
-		dead = insyscall = false;
+		son.dead = son.insyscall = false;
 		sig = exit_code = 0;
 		for (;;) {
 			/* At this point the traced child is stopped and needs
 			 * to be resumed.
 			 */
-			if (!pink_trace_syscall(pid, sig)) {
+			if (!pink_trace_syscall(son.pid, sig)) {
 				perror("pink_trace_syscall");
 				return (errno == ESRCH) ? 0 : 1;
 			}
 			sig = 0;
 
 			/* Wait for the child */
-			if ((pid = waitpid(pid, &status, 0)) < 0) {
+			if ((son.pid = waitpid(son.pid, &status, 0)) < 0) {
 				perror("waitpid");
 				return (errno == ECHILD) ? 0 : 1;
 			}
@@ -178,32 +205,17 @@ main(int argc, char **argv)
 			event = pink_event_decide(status);
 			switch (event) {
 			case PINK_EVENT_SYSCALL:
-				/* We get this event twice, one at entering a
-				 * system call and one at exiting a system
-				 * call. */
-				if (insyscall) {
-					/* Exiting the system call, print the
-					 * return value. */
-					fputc(' ', stdout);
-					print_ret(pid);
-					fputc('\n', stdout);
-					insyscall = false;
-				}
-				else {
-					/* Get the system call number and call
-					 * the appropriate decoder. */
-					if (!pink_util_get_syscall(pid, &scno))
-						perror("pink_util_get_syscall");
-					else if (scno == SYS_open)
-						decode_open(pid, bitness);
-					else
-						decode_simple(bitness, scno);
-					insyscall = true;
-				}
+				handle_syscall(&son);
+				break;
 				break;
 			case PINK_EVENT_EXEC:
 				/* Update bitness */
-				bitness = pink_bitness_get(pid);
+				son.bitness = pink_bitness_get(son.pid);
+				if (son.bitness == PINK_BITNESS_UNKNOWN)
+					err(EXIT_FAILURE, "pink_bitness_get");
+				else
+					printf(" (Updating the bitness of child %i to %s mode)\n",
+						son.pid, pink_bitness_tostring(son.bitness));
 				break;
 			case PINK_EVENT_GENUINE:
 			case PINK_EVENT_UNKNOWN:
@@ -215,20 +227,20 @@ main(int argc, char **argv)
 			case PINK_EVENT_EXIT_GENUINE:
 				exit_code = WEXITSTATUS(status);
 				printf("Child %i exited normally with return code %d\n",
-						pid, exit_code);
-				dead = true;
+						son.pid, exit_code);
+				son.dead = true;
 				break;
 			case PINK_EVENT_EXIT_SIGNAL:
 				exit_code = 128 + WTERMSIG(status);
 				printf("Child %i exited with signal %d\n",
-						pid, WTERMSIG(status));
-				dead = true;
+						son.pid, WTERMSIG(status));
+				son.dead = true;
 				break;
 			default:
 				/* Nothing */
 				;
 			}
-			if (dead)
+			if (son.dead)
 				break;
 		}
 

include/pinktrace/util.h
@@ -247,23 +247,25 @@ pink_util_putn_safe(pid_t pid, long addr, const char *src, size_t len);
  * Gets the last system call called by child with the given process ID.
  *
  * \param pid Process ID of the child whose system call is to be returned.
+ * \param bitness Bitness of the child
  * \param res Pointer to store the result.
  *
  * \return true on success, false on failure and sets errno accordingly.
  **/
 bool
-pink_util_get_syscall(pid_t pid, long *res);
+pink_util_get_syscall(pid_t pid, pink_bitness_t bitness, long *res);
 
 /**
  * Sets the system call to the given value.
  *
  * \param pid Process ID of the child whose system call is to be set.
+ * \param bitness Bitness of the child
  * \param scno System call to set.
  *
  * \return true on success, false on failure and sets errno accordingly.
  **/
 bool
-pink_util_set_syscall(pid_t pid, long scno);
+pink_util_set_syscall(pid_t pid, pink_bitness_t bitness, long scno);
 
 /**
  * Gets the return value of the last system call called by child with the given

python/pinktrace/pink-python-syscall.c
@@ -65,6 +65,7 @@ static char pinkpy_syscall_get_no_doc[] = ""
 	"Returns the last system call number called by the traced child.\n"
 	"\n"
 	"@param pid: Process ID of the traced child\n"
+	"@param bitness: Bitness of the traced child\n"
 	"@raise OSError: Raised when the underlying ptrace call fails.\n"
 	"@rtype: long\n"
 	"@return: The number of the system call";
@@ -72,12 +73,17 @@ static PyObject *
 pinkpy_syscall_get_no(pink_unused PyObject *self, PyObject *args)
 {
 	pid_t pid;
+	pink_bitness_t bit;
 	long scno;
 
-	if (!PyArg_ParseTuple(args, PARSE_PID, &pid))
+	bit = PINKTRACE_DEFAULT_BITNESS;
+	if (!PyArg_ParseTuple(args, PARSE_PID"|I", &pid, &bit))
 		return NULL;
 
-	if (!pink_util_get_syscall(pid, &scno))
+	if (!check_bitness(bit))
+		return NULL;
+
+	if (!pink_util_get_syscall(pid, bit, &scno))
 		return PyErr_SetFromErrno(PyExc_OSError);
 
 	return Py_BuildValue("l", scno);
@@ -88,17 +94,23 @@ static char pinkpy_syscall_set_no_doc[] = ""
 	"\n"
 	"@param pid: Process ID of the traced child\n"
 	"@param scno: The number of the system call\n"
+	"@param bitness: Bitness of the traced child\n"
 	"@raise OSError: Raised when the underlying ptrace call fails.\n";
 static PyObject *
 pinkpy_syscall_set_no(pink_unused PyObject *self, PyObject *args)
 {
 	pid_t pid;
+	pink_bitness_t bit;
 	long scno;
 
-	if (!PyArg_ParseTuple(args, PARSE_PID"l", &pid, &scno))
+	bit = PINKTRACE_DEFAULT_BITNESS;
+	if (!PyArg_ParseTuple(args, PARSE_PID"l|I", &pid, &scno, &bit))
 		return NULL;
 
-	if (!pink_util_set_syscall(pid, scno))
+	if (!check_bitness(bit))
+		return NULL;
+
+	if (!pink_util_set_syscall(pid, bit, scno))
 		return PyErr_SetFromErrno(PyExc_OSError);
 
 	return Py_BuildValue("");

ruby/TEST_syscall_freebsd.rb
@@ -100,13 +100,16 @@ class TestPinkSyscall < Test::Unit::TestCase
       PinkTrace::Syscall.set_no 0
     end
     assert_raise ArgumentError do
-      PinkTrace::Syscall.set_no 0, 1, 2
+      PinkTrace::Syscall.set_no 0, 1, 2, 3
     end
     assert_raise TypeError do
       PinkTrace::Syscall.set_no 'pink', 1
     end
     assert_raise TypeError do
       PinkTrace::Syscall.set_no 0, 'pink'
+    end
+    assert_raise PinkTrace::BitnessError do
+      PinkTrace::Syscall.set_no 0, 1, 13
     end
   end
 

ruby/pink-ruby.c
@@ -886,22 +886,37 @@ pinkrb_name_syscall(int argc, VALUE *argv, pink_unused VALUE mod)
 
 /*
  * Document-method: PinkTrace::Syscall.get_no
- * call-seq: PinkTrace::Syscall.get_no(pid) => fixnum
+ * call-seq: PinkTrace::Syscall.get_no(pid, [bitness=PinkTrace::Bitness::DEFAULT]) => fixnum
  *
  * Returns the last system call number called by the traced child.
  */
 static VALUE
-pinkrb_util_get_syscall(pink_unused VALUE mod, VALUE pidv)
+pinkrb_util_get_syscall(int argc, VALUE *argv, pink_unused VALUE mod)
 {
 	pid_t pid;
+	unsigned bit;
 	long scno;
 
-	if (FIXNUM_P(pidv))
-		pid = FIX2INT(pidv);
+	if (argc < 1 || argc > 2)
+		rb_raise(rb_eArgError, "Wrong number of arguments");
+
+	if (FIXNUM_P(argv[0]))
+		pid = FIX2LONG(argv[0]);
 	else
 		rb_raise(rb_eTypeError, "First argument is not a Fixnum");
 
-	if (!pink_util_get_syscall(pid, &scno))
+	if (argc == 2) {
+		if (FIXNUM_P(argv[1])) {
+			bit = FIX2UINT(argv[1]);
+			check_bitness(bit);
+		}
+		else
+			rb_raise(rb_eTypeError, "Second argument is not a Fixnum");
+	}
+	else
+		bit = PINKTRACE_DEFAULT_BITNESS;
+
+	if (!pink_util_get_syscall(pid, bit, &scno))
 		rb_sys_fail("pink_util_get_syscall()");
 
 	return LONG2FIX(scno);
@@ -909,27 +924,42 @@ pinkrb_util_get_syscall(pink_unused VALUE mod, VALUE pidv)
 
 /*
  * Document-method: PinkTrace::Syscall.set_no
- * call-seq: PinkTrace::Syscall.set_no(pid, scno) => nil
+ * call-seq: PinkTrace::Syscall.set_no(pid, scno, [bitness=PinkTrace::Bitness::DEFAULT]) => nil
  *
  * Sets the system call number for the traced child.
  */
 static VALUE
-pinkrb_util_set_syscall(pink_unused VALUE mod, VALUE pidv, VALUE scnov)
+pinkrb_util_set_syscall(int argc, VALUE *argv, pink_unused VALUE mod)
 {
 	pid_t pid;
+	unsigned bit;
 	long scno;
 
-	if (FIXNUM_P(pidv))
-		pid = FIX2INT(pidv);
+	if (argc < 2 || argc > 3)
+		rb_raise(rb_eArgError, "Wrong number of arguments");
+
+	if (FIXNUM_P(argv[0]))
+		pid = FIX2INT(argv[0]);
 	else
 		rb_raise(rb_eTypeError, "First argument is not a Fixnum");
 
-	if (FIXNUM_P(scnov))
-		scno = FIX2LONG(scnov);
+	if (FIXNUM_P(argv[1]))
+		scno = FIX2LONG(argv[1]);
 	else
 		rb_raise(rb_eTypeError, "Second argument is not a Fixnum");
 
-	if (!pink_util_set_syscall(pid, scno))
+	if (argc == 3) {
+		if (FIXNUM_P(argv[2])) {
+			bit = FIX2UINT(argv[2]);
+			check_bitness(bit);
+		}
+		else
+			rb_raise(rb_eTypeError, "Second argument is not a Fixnum");
+	}
+	else
+		bit = PINKTRACE_DEFAULT_BITNESS;
+
+	if (!pink_util_set_syscall(pid, bit, scno))
 		rb_sys_fail("pink_util_set_syscall()");
 
 	return Qnil;
@@ -1730,8 +1760,8 @@ Init_PinkTrace(void)
 	/* util.h && name.h */
 	syscall_mod = rb_define_module_under(mod, "Syscall");
 	rb_define_module_function(syscall_mod, "name", pinkrb_name_syscall, -1);
-	rb_define_module_function(syscall_mod, "get_no", pinkrb_util_get_syscall, 1);
-	rb_define_module_function(syscall_mod, "set_no", pinkrb_util_set_syscall, 2);
+	rb_define_module_function(syscall_mod, "get_no", pinkrb_util_get_syscall, -1);
+	rb_define_module_function(syscall_mod, "set_no", pinkrb_util_set_syscall, -1);
 	rb_define_module_function(syscall_mod, "get_ret", pinkrb_util_get_return, 1);
 	rb_define_module_function(syscall_mod, "set_ret", pinkrb_util_set_return, 2);
 	rb_define_module_function(syscall_mod, "get_arg", pinkrb_util_get_arg, -1);

src/pink-freebsd-trace-amd64.c
@@ -23,22 +23,54 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <sys/syscall.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/syscall.h>
+#include <sys/sysctl.h>
 
 #include <pinktrace/internal.h>
 #include <pinktrace/pink.h>
 
+struct bitness_types {
+	const char *type;
+	pink_bitness_t bitness;
+} bitness_types[] = {
+	{"FreeBSD ELF64", PINK_BITNESS_64},
+	{"FreeBSD ELF32", PINK_BITNESS_32},
+	{NULL, -1}
+};
+
 pink_bitness_t
-pink_bitness_get(pink_unused pid_t pid)
+pink_bitness_get(pid_t pid)
 {
-	return PINK_BITNESS_64;
+	char progt[32];
+	size_t len = sizeof(progt);
+	int mib[4];
+	struct bitness_types *walk;
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_SV_NAME;
+	mib[3] = pid;
+
+	if (sysctl(mib, 4, progt, &len, NULL, 0) < 0)
+		return PINK_BITNESS_UNKNOWN;
+
+	for (walk = bitness_types; walk->type; walk++) {
+		if (!strcmp(walk->type, progt))
+			return walk->bitness;
+	}
+
+	return PINK_BITNESS_UNKNOWN;
 }
 
 bool
-pink_util_get_syscall(pid_t pid, long *res)
+pink_util_get_syscall(pid_t pid, pink_bitness_t bitness, long *res)
 {
+	long parm_offset;
 	struct reg r;
+
+	assert(bitness == PINK_BITNESS_32 || bitness == PINK_BITNESS_64);
 
 	if (!pink_util_get_regs(pid, &r))
 		return false;
@@ -52,7 +84,13 @@ pink_util_get_syscall(pid_t pid, long *res)
 	switch (*res) {
 	case SYS_syscall:
 	case SYS___syscall:
-		*res = r.r_rdi;
+		if (bitness == PINK_BITNESS_32) {
+			parm_offset = r.r_rsp + sizeof(int);
+			if (!pink_util_peekdata(pid, parm_offset, res))
+				return false;
+		}
+		else
+			*res = r.r_rdi;
 		return true;
 	default:
 		return true;
@@ -60,7 +98,7 @@ pink_util_get_syscall(pid_t pid, long *res)
 }
 
 bool
-pink_util_set_syscall(pid_t pid, long scno)
+pink_util_set_syscall(pid_t pid, pink_unused pink_bitness_t bitness, long scno)
 {
 	struct reg r;
 
@@ -114,11 +152,12 @@ pink_util_set_return(pid_t pid, long ret)
 }
 
 bool
-pink_util_get_arg(pid_t pid, pink_unused pink_bitness_t bitness, unsigned ind, long *res)
+pink_util_get_arg(pid_t pid, pink_bitness_t bitness, unsigned ind, long *res)
 {
 	unsigned parm_offset;
 	struct reg r;
 
+	assert(bitness == PINK_BITNESS_32 || bitness == PINK_BITNESS_64);
 	assert(ind < PINK_MAX_INDEX);
 
 	if (!pink_util_get_regs(pid, &r))
@@ -129,13 +168,29 @@ pink_util_get_arg(pid_t pid, pink_unused pink_bitness_t bitness, unsigned ind, l
 	 * SYS_syscall, and SYS___syscall.  The former is the old syscall()
 	 * routine, basicly; the latter is for quad-aligned arguments.
 	 */
+	parm_offset = r.r_rsp + (bitness == PINK_BITNESS_32 ? sizeof(int) : sizeof(register_t));
 	switch (r.r_rax) {
 	case SYS_syscall:
+		if (bitness == PINK_BITNESS_64)
+			++ind;
+		else
+			parm_offset += sizeof(int);
+		break;
 	case SYS___syscall:
-		++ind;
+		if (bitness == PINK_BITNESS_64)
+			++ind;
+		else
+			parm_offset += sizeof(quad_t);
 		break;
 	default:
 		break;
+	}
+
+	if (bitness == PINK_BITNESS_32) {
+		parm_offset += ind * sizeof(int);
+		if (!pink_util_peekdata(pid, parm_offset, res))
+			return false;
+		return true;
 	}
 
 	switch (ind) {
@@ -159,7 +214,6 @@ pink_util_get_arg(pid_t pid, pink_unused pink_bitness_t bitness, unsigned ind, l
 		break;
 	case 6:
 		/* system call redirection */
-		parm_offset = r.r_rsp + sizeof(register_t);
 		if (!pink_util_peekdata(pid, parm_offset, res))
 			return false;
 		break;

commit 65f4b39ff7e80edbb48885060b8b48095c4057de

freebsd/i386: Fix compilation, broken by 5ff446d

src/pink-freebsd-trace-i386.c
@@ -63,7 +63,7 @@ pink_util_get_syscall(pid_t pid, pink_unused pink_bitness_t bitness, long *res)
 }
 
 bool
-pink_util_set_syscall(pid_t pid, long scno)
+pink_util_set_syscall(pid_t pid, pink_unused pink_bitness_t bitness, long scno)
 {
 	struct reg r;
 