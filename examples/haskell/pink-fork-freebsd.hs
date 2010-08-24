07f29da832453439ca4ce3e7b223a3f13a7a238d
Ali Polatel <alip@exherbo.org>
2010-08-24 12:59:53 +0300

    haskell: new example pink-fork-freebsd.hs

---
+++ examples/haskell/pink-fork-freebsd.hs
@@ -0,0 +1,30 @@
+-- A simple example showing how to do the tracing fork on FreeBSD
+-- vim: set ft=haskell et ts=4 sts=4 sw=4 fdm=marker :
+
+import Data.Maybe
+import Control.Monad
+import System.Exit
+import System.Posix.Process
+import System.Posix.Signals
+import System.PinkTrace.Trace
+
+main :: IO ()
+main = do
+    pid <- forkProcess child
+    status <- getProcessStatus True False pid
+    case fromJust status of
+        Exited (ExitFailure ret) -> putStrLn ("Child exited with code: " ++ (show ret)) >> exitFailure
+        Exited ExitSuccess       -> putStrLn ("Child exited with code: 0") >> exitFailure
+        Terminated sig           -> putStrLn ("Child was terminated: " ++ (show sig)) >> exitFailure
+        Stopped sig              -> unless (sig == softwareStop)
+                                        (putStrLn ("Wrong signal: " ++ show sig) >> exitFailure)
+
+    -- Nothing interesting in this example.
+    -- Let the child continue its execution.
+    traceContinue pid nullSignal 1
+    where
+        child :: IO ()
+        child = do
+            traceMe
+            raiseSignal softwareStop
+            putStrLn "hello world"
