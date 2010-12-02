81baee7fee1e2d7ab170179b7ef49f970c0b463d
Ali Polatel <alip@exherbo.org>
2010-12-02 21:56:20 +0200

    haskell: Tweak haddock.sh

--- haddock.sh
+++ haddock.sh
@@ -5,12 +5,26 @@
 HOST="bach.exherbo.org"
 DESTINATION="public_html/pinktrace/api/haskell"
 HACKAGE='http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html'
-OUTPUT_DIRECTORY="dist/doc/html/pinktrace"
 
-cabal haddock --html-location="${HACKAGE}" || exit 1
+if [ ! -f ./Setup.lhs ]; then
+    echo "./configure" >&2
+    exit 1
+fi
 
-rsync -avze ssh \
+if [ ! -d ./dist ]; then
+    echo "runhaskell Setup.lhs build" >&2
+    exit 1
+fi
+
+echo ">>> runhaskell Setup.lhs haddock"
+runhaskell Setup.lhs haddock \
+    "--haddock-options=--html --use-unicode" \
+    --html-location="${HACKAGE}" || exit 1
+
+echo ">>> Uploading to ${HOST}:${DESTINATION}"
+rsync \
+    -avze ssh \
     --delete \
     --partial \
     --progress \
-    ${OUTPUT_DIRECTORY}/* "${HOST}":"${DESTINATION}"/ || exit 2
+    ./dist/doc/html/pinktrace/ "${HOST}":"${DESTINATION}" || exit 1
