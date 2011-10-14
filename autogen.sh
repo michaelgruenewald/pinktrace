96b958668d713678599165730e56febd96b14d9b
Ali Polatel <alip@exherbo.org>
2011-10-15 00:19:19 +0300

    autotools: various enhancements

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
-# vim: set sw=4 et sts=4 tw=80 :
 
-die() {
-    echo "$@" >&2
-    exit 1
-}
+set -e
+set -x
 
-echo ">>> libtoolize --copy --force --automake"
-libtoolize --copy --force --automake || die "libtoolize failed"
-echo ">>> rm -f config.cache"
+rm -fr autom4te.cache
 rm -f config.cache
-echo ">>> aclocal -I m4"
-aclocal -I m4 || die "aclocal failed"
-echo ">>> autoheader"
-autoheader || die "autoheader failed"
-echo ">>> autoconf"
-autoconf || die "autoconf failed"
-echo ">>> automake --foreign --add-missing --copy"
-automake --foreign --add-missing --copy || die "automake failed"
+test -d build-aux || mkdir build-aux
+
+libtoolize --copy --force --automake
+aclocal -I m4
+autoheader
+autoconf -Wall
+automake --add-missing --copy --foreign
--- m4/ipv6.m4
+++ m4/ipv6.m4
@@ -3,12 +3,12 @@ dnl Based in part upon netmap-5.00
 
 AC_DEFUN([APR_CHECK_SOCKADDR_IN6],[
 AC_CACHE_CHECK(for sockaddr_in6, ac_cv_define_sockaddr_in6,[
-AC_TRY_COMPILE([
+AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[
 #include <sys/types.h>
 #include <netinet/in.h>
-],[
+]], [[
 struct sockaddr_in6 sa;
-],[
+]])],[
     ac_cv_define_sockaddr_in6=yes
 ],[
     ac_cv_define_sockaddr_in6=no
@@ -24,13 +24,13 @@ fi
 
 AC_DEFUN([CHECK_AF_INET6_DEFINE],[
 AC_CACHE_CHECK(for AF_INET6 definition, ac_cv_define_af_inet6,[
-AC_TRY_COMPILE([
+AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-],[
+]], [[
 int af = AF_INET6;
-],[
+]])],[
     ac_cv_define_af_inet6=yes
 ],[
     ac_cv_define_af_inet6=no
